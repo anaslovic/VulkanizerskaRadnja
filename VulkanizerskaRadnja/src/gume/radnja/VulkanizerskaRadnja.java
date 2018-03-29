3e773b393972e041c2029d86e8ad4be6ffb8a0c8
anaslovic <slovicana0@gmail.com>
2018-03-30 01:54:17 +0200

    Napisana detaljna dokumentacija za klasu VulkanizerskaRadnja

--- VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
@@ -4,10 +4,26 @@ import java.util.LinkedList;
 
 import gume.AutoGuma;
 
+/**
+ * 
+ * Klasa predstavlja vulkanizersku radnju sa najosnovnijim funkcionalnostima
+ * @author Ana Slovic
+ * @version 1.0.0
+ * 
+ */
 public class VulkanizerskaRadnja {
 	
+	/**
+	 * Lista automobilskih guma koje se nalaze u asortimanu radnje
+	 */
 	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
 
+	/**
+	 * Metoda dodaje novu automobilsku gumu u asortiman radnje
+	 * @param a Nova vrsta gume koja se dodaje u ponudu
+	 * @throws java.lang.NullPointerException ako je uneta guma null
+	 *  @throws java.lang.RuntimeException ako uneta guma vec postoji u ponudi
+	 */
 	public void dodajGumu(AutoGuma a) {
 		if (a == null)
 			throw new NullPointerException("Guma ne sme biti null");
@@ -15,7 +31,16 @@ public class VulkanizerskaRadnja {
 			throw new RuntimeException("Guma vec postoji");
 		gume.addFirst(a);
 	}
-
+	
+	/**
+	 * Pretrazuje celokupnu ponudu po marki i modelu i 
+	 * vraca listu svih guma cija se vrednost tog atributa poklapa
+	 * 
+	 * @param markaModel Marka i model trazene gume
+	 * 
+	 * @return lista guma koje su trazene marke i modela
+	 * 
+	 */
 	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
 		if (markaModel == null)
 			return null;
